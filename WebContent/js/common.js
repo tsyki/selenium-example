a5ee45fef4bba34f066f0c4157fd1a12fcfb3563
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2017-03-26 17:34:37 +0900

    値変更メソッドをリファクタ。disabled変更メソッドを追加

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -1,3 +1,17 @@
+var url="NopService";
+
+function asyncChangeValue(targetId,value){
+	$.post(url,targetId,function(result){
+		$("#" + targetId).val(value);
+	});
+}
+
+function asyncChangeDisabled(targetId,value){
+	$.post(url,targetId,function(result){
+		$("#hoge").prop('disabled', value);
+	});
+}
+
 function sendAjaxRequest(target){
 	var url="NopService";
 	$.post(url,target,postCallBack);
